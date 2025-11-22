cd541ccc6505cd841584f2d2380570ba14984f7e
VIXAY VONGPUNYA <vongpunyavixay@gmail.com>
2025-11-22 21:49:41 +0900

    initial

---
+++ _next/static/chunks/bc9c3264-1faf4237f52d5d79.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[263],{4796:(t,a,h)=>{h.d(a,{E5v:()=>v});var e=h(1538);function v(t){return(0,e.k5)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M5 20h14v-2H5v2zM19 9h-4V3H9v6H5l7 7 7-7z"}}]})(t)}}}]);
